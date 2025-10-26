ccfbbea6002b695525eb09c02d0e468d9ceb4f6a
PandeyChhavi <chhaviii@gmail.com>
2025-10-26 09:39:20 -0500

    fixed run pipline

--- data-science/src/train.py
+++ data-science/src/train.py
@@ -23,7 +23,7 @@ def parse_args():
     parser.add_argument("--train_data", type=str, help="Path to train dataset")  # Specify the type for train_data
     parser.add_argument("--test_data", type=str, help="Path to test dataset")  # Specify the type for test_data
     parser.add_argument("--model_output", type=str, help="Path of output model")  # Specify the type for model_output
-    parser.add_argument('--n_estimators', type=float, default=100,
+    parser.add_argument('--n_estimators', type=float, default=100.0,
                         help='The number of trees in the forest')  # Specify the type and default value for n_estimators
     parser.add_argument('--max_depth', type=float, default=None,
                         help='The maximum depth of the tree')  # Specify the type and default value for max_depth
